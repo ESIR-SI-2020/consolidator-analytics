559e4a3a9b89b464f5fcc88d0a6415d64757be9f
Matthias <matthiaspinard@gmail.com>
2019-10-08 14:17:30 +0200

    Test kafka

---
+++ src/main/java/TestConsumerKafka.java
@@ -0,0 +1,31 @@
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.MockConsumer;
+import org.apache.kafka.clients.consumer.OffsetResetStrategy;
+import org.apache.kafka.common.TopicPartition;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class TestConsumerKafka {
+    public static String TOPIC = "user";
+
+    public void read() {
+        TopicPartition topic = new TopicPartition(TOPIC, 0);
+
+        MockConsumer<Long, String> consumer = new MockConsumer<Long, String>(OffsetResetStrategy.EARLIEST);
+        consumer.assign(Collections.singletonList(topic));
+
+        HashMap<TopicPartition, Long> beginningOffsets = new HashMap<>();
+        beginningOffsets.put(topic, 0L);
+        consumer.updateBeginningOffsets(beginningOffsets);
+
+        consumer.addRecord(new ConsumerRecord<>(TOPIC, 0, 0, 4141L, "test"));
+        ConsumerRecords<Long, String> records = consumer.poll(Duration.ofMillis(1000L));
+        System.out.println("count: " + records.count());
+        records.forEach(record -> {
+            System.out.println(record.value());
+        });
+    }
+}
