5ad9e01cf4e984b3ce5668b0036602a5edf0b07c
Matthias <matthiaspinard@gmail.com>
2019-10-21 14:11:02 +0200

    Ajout d'un id au modèles

--- src/main/java/com/esir/jxc/mpc/model/ArticleAdded.java
+++ src/main/java/com/esir/jxc/mpc/model/ArticleAdded.java
@@ -1,14 +1,16 @@
 package com.esir.jxc.mpc.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 import java.util.Date;
 
 @Data
-@Document(indexName = "articleadded")
+@Document(indexName = "pocket", type="article_added")
 public class ArticleAdded {
-
+    @Id
+    private String id;
     private Date creationDate;
 
 }
--- src/main/java/com/esir/jxc/mpc/model/UserAdded.java
+++ src/main/java/com/esir/jxc/mpc/model/UserAdded.java
@@ -1,14 +1,16 @@
 package com.esir.jxc.mpc.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 import java.util.Date;
 
 @Data
-@Document(indexName = "useradded")
+@Document(indexName = "pocket", type="article_added")
 public class UserAdded {
-
+    @Id
+    private String id;
     private Date creationDate;
 
 }
