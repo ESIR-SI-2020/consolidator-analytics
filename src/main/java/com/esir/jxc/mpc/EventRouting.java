4981ce3875f7fcdc33aab630d58891b35fb9a31c
Glouglou <clementbourgeault@gmail.com>
2019-10-21 15:41:37 +0200

    routing article-added

--- src/main/java/com/esir/jxc/mpc/EventRouting.java
+++ src/main/java/com/esir/jxc/mpc/EventRouting.java
@@ -3,6 +3,7 @@ package com.esir.jxc.mpc;
 import com.esir.jxc.mpc.model.Event;
 import com.esir.jxc.mpc.model.UserAdded;
 import com.esir.jxc.mpc.model.ArticleAdded;
+import com.esir.jxc.mpc.model.event.EventArticleCreated;
 import com.esir.jxc.mpc.repository.ArticleAddedRepository;
 import com.esir.jxc.mpc.repository.UserAddedRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,13 @@ public class EventRouting {
         }
 
         else if (event.getEventName().equals("ARTICLE_ADDED")) {
+
+            EventArticleCreated eventArticleCreated = EventArticleCreated.of(event);
             ArticleAdded articleAdded = new ArticleAdded();
             articleAdded.setCreationDate(getDate());
+            articleAdded.setUrl(eventArticleCreated.getUrl());
             articleAddedRepository.save(articleAdded);
+
         }
     }
 
