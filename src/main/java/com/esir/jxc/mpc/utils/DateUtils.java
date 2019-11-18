9d4e6b172d7f97fd329d5d4e5baf0011de21136c
clementBourgeault <clementbourgeault@gmail.com>
2019-11-18 17:53:54 +0100

    add date utils

---
+++ src/main/java/com/esir/jxc/mpc/utils/DateUtils.java
@@ -0,0 +1,16 @@
+package com.esir.jxc.mpc.utils;
+
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateUtils {
+
+    public static Date getDate() {
+        LocalDateTime now = LocalDateTime.now();
+        Calendar cal = Calendar.getInstance();
+        cal.set(now.getYear(), now.getMonthValue(), now.getDayOfMonth());
+        return cal.getTime();
+    }
+
+}
