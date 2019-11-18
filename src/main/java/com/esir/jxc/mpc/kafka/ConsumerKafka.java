5af31bb155b81872fbb97dd2ccac959a5cd6937d
Matthias <matthiaspinard@gmail.com>
2019-11-18 16:48:35 +0100

    Utilisation de Kafka spring

--- src/main/java/com/esir/jxc/mpc/kafka/ConsumerKafka.java
+++ src/main/java/com/esir/jxc/mpc/kafka/ConsumerKafka.java
@@ -1,11 +1,13 @@
-package com.esir.jxc.mpc;
+package com.esir.jxc.mpc.kafka;
 
+import com.esir.jxc.mpc.EventRouting;
 import com.esir.jxc.mpc.model.Event;
 import org.apache.kafka.clients.producer.Producer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -13,11 +15,12 @@ import java.io.IOException;
 @Service
 public class ConsumerKafka {
 
-    @Autowired EventRouting eventRouting;
+    @Autowired
+    EventRouting eventRouting;
     private final Logger logger = LoggerFactory.getLogger(Producer.class);
 
-    @KafkaListener(topics = "users", groupId = "esir2019")
-    public void consume(Event event) throws IOException {
+    @KafkaListener(topics = "users", containerFactory = "kafkaListenerContainerFactory")
+    public void consume(@Payload Event event) throws IOException {
         logger.info(String.format("#### -> Consumed message -> %s", event.getEventName()));
         eventRouting.processEvent(event);
     }
