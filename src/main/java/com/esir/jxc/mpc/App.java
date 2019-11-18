dc08a4e2ee8c7468f23bfaf9e02093925037438f
clementBourgeault <clementbourgeault@gmail.com>
2019-11-18 17:13:05 +0100

    post-PR modifications

--- src/main/java/com/esir/jxc/mpc/App.java
+++ src/main/java/com/esir/jxc/mpc/App.java
@@ -6,18 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-            System.out.println("Application starting...");
-
-        };
     }
 }
